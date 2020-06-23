acc23e39af4bb9cc0d4105e805603ea358d6c143
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-23 09:54:31 -0300

    First commit.

---
+++ Cartao.cs
@@ -0,0 +1,13 @@
+namespace Aula23List
+{
+    public class Cartao
+    {
+        public string Titular { get; set; }
+        public int Numero { get; set; }
+        public string Bandeira { get; set; }
+        public int Vencimento { get; set; }
+        public int Cvv { get; set; }
+
+        
+    }
+}
